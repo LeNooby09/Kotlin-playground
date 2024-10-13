a3dada00492d983b6212e512312d005dd1f0601f
LeNooby09 <phillihp.steingraeber@proton.me>
2024-10-13 13:28:36 +0200

    oneliner
    Signed-off-by: LeNooby09 <phillihp.steingraeber@proton.me>

---
+++ src/main/kotlin/nanam's_tweet_oneliner.kt
@@ -0,0 +1,1 @@
+fun Application.configureRouting(){routing{get("/"){call.respondHtml{head{title{+"Nyaaaaa"}}body{h1{+"Hellonyaaaaaaa"}h3{+"Finally I made a ktor example thing :3"}p{+"Go to "a{href = "/tmdb"+"/tmdb"}+" to see the Tmdb client in action"}p{+"Go to "a{href = "/Lucy"+"/Lucy"}+" to play Lucy roulette"}}}}}}
